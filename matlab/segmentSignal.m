2906162bcd11757d8553e1fe39a61a4fb8fc4f53
Basheq Tarifi <btarifi08@gmail.com>
2021-09-16 14:50:16 +0200

    Segment + Visualise regioned Data

--- matlab/segmentSignal.m
+++ matlab/segmentSignal.m
@@ -1,4 +1,4 @@
-function [segmentedPeriods, indices] = segmentSignal(data, numPeriodsPerSegment, keepWholeSignal)
+function [segmentedPeriods, indices] = segmentSignal(data, numPeriodsPerSegment,  colOfTarget, keepWholeSignal)
 
 % Segments a signal into a cells which contain a specified number of cycles
 % of that signal.
@@ -9,28 +9,33 @@ function [segmentedPeriods, indices] = segmentSignal(data, numPeriodsPerSegment,
 if (nargin < 2)
     error('Please provide data and number of periods per segment')
 end
-
 if (nargin < 3)
+   colOfTarget = 1;
+end
+if (nargin < 4)
     keepWholeSignal = false;
 end
 
 segmentedPeriods = {};
 indices = [];
 
-[~,locsOfMin] = findpeaks(-data, 'MinPeakDistance',60);
-disp(length(locsOfMin))
+target = data(:,colOfTarget);
+
+[~,locsOfMin] = findpeaks(-target, 'MinPeakDistance',60);
+%disp(length(locsOfMin))
 for l = 1:numPeriodsPerSegment:(length(locsOfMin)-numPeriodsPerSegment)
     first = locsOfMin(l);
     last = locsOfMin(l + numPeriodsPerSegment) - 1;
-    segmentedPeriods{end+1} = data(first:last);
+    segmentedPeriods{end+1} = data(first:last, :);
     indices(end+1) = first;
 end
 
+% Preserve incomplete segments with not enough periods at the end of the signal
 if (keepWholeSignal)
     if (numPeriodsPerSegment*length(segmentedPeriods) < length(locsOfMin))
         first = locsOfMin(numPeriodsPerSegment*length(segmentedPeriods)+1);
         last = locsOfMin(end);
-        segmentedPeriods{end+1} = data(first:last);
+        segmentedPeriods{end+1} = data(first:last, :);
         indices(end+1) = first;
     end
 end
