c1031393970a9da8547992cd75b0a0d2a6940bca
Basheq Tarifi <btarifi08@gmail.com>
2021-09-16 11:44:52 +0200

    Complete ABP detection and incorporate with filter file
    - Rename "remove flats" to "remove invalid"
    - Close files when complete

---
+++ matlab/findInvalidABPRegions.m
@@ -0,0 +1,28 @@
+function invalidRegions = findInvalidABPRegions(abpSignal, locationsOfAbpBeats, tolerance)
+% Uses the data from the WFDB WABP function which detects valid ABP signals
+% to find invalid regions in the ABP signal. Performs this by mapping the
+% locations to the signals in a logical array and then taking the maximum
+% and then taking the maximum within a sliding window.
+% invalidRegions is a vector the length of the ABP signal where 1 = invalid
+% region and 0 = valid region
+
+if (nargin < 2)
+    error('Please provide ABP signal and locations of beats as extracted from wabp annotation.');
+elseif (nargin < 3)
+    tolerance = 120;
+end
+    
+
+beatLocs = zeros(size(abpSignal));
+% Why are there sample numbers greater than the size of the signal??
+locationsOfAbpBeats(locationsOfAbpBeats > length(abpSignal)) = [];
+
+beatLocs(locationsOfAbpBeats) = 1;
+beatLocs(length(abpSignal)+1:end) = [];
+
+invalidRegions = ~movmax(beatLocs,[tolerance tolerance]);
+
+plot(invalidRegions);
+
+end
+
---
+++ matlab/removeInvalidFromSignal.m
@@ -0,0 +1,46 @@
+function [validRegions, num_regions] = removeInvalidFromSignal(invalidRegions, minimumSegmentLength, time, signal1, signal2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+if( length(invalidRegions)~=length(signal1) ||  length(invalidRegions)~=length(signal2))
+    error("Signal lengths not all equal");
+end
+
+if ~any(invalidRegions) %if there are no flats in the signal
+    validRegions = {{time},{signal1},{signal2}};
+end
+
+%use |diff| to find changes between normal regions and flat (regions between
+%two spikes are flat)
+spike_indices = [find( [0; abs(diff(invalidRegions))] )]; %0 added to account for last of first index when taking diff
+
+
+%need to find first valid index -> first index that is a zero in
+%flatRegions
+first_idx = find(1-invalidRegions, 1, 'first');
+if first_idx == 1
+    spike_indices = [1; spike_indices; length(invalidRegions)];
+else 
+    spike_indices = [first_idx; spike_indices(2:end); length(invalidRegions)];
+end
+
+
+validRegions = {{},{},{}};
+num_regions = 0;
+
+% extract non flats regions
+% remove regions that are too small (<minimumSegmentLength)
+for i=1:2:length(spike_indices)-1
+    if spike_indices(i+1)-spike_indices(i) < minimumSegmentLength
+        continue;
+    end
+    
+    num_regions = num_regions+1;
+    idx1 = spike_indices(i);
+    idx2 = spike_indices(i+1);
+    validRegions{num_regions} = { {time(idx1:idx2)}, {signal1(idx1:idx2)}, { signal2(idx1:idx2) } };
+    
+end
+
+end
+
