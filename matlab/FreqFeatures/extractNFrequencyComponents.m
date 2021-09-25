6fd7ebcd06226d5f79498ae10fd881ebe9c2ce7e
Aaron Fainman <aaronfainman@gmail.com>
2021-09-25 11:14:34 +0200

    ...

---
+++ matlab/FreqFeatures/extractNFrequencyComponents.m
@@ -0,0 +1,28 @@
+function [pkIndices, pkFreqs, pkMags, pkPhases, bandwidth] = extractNFrequencyComponents(t, x, numComponents, phasesPerPeak, bandwidth_criterion)
+[f, fftX] = singlesidedFFT(t, x);
+
+psdX = (abs(fftX)).^2;
+psdX(2:end) = psdX(2:end)/2;
+
+pkIndices = [1; extractMaxFreqSpectrum(abs(psdABP), numComponents, 0)]
+
+pkAvgWidth = mean(widths)* mean(diff(f));
+
+% pkIndices(end+1) = 1;
+
+pkIndices = sort(pkIndices);
+
+pkFreqs = f(pkIndices)';
+pkMags = abs(fftX(pkIndices));
+
+pkPhases = angle(fftX(pkIndices));
+
+fftLength = length(fftX);
+
+power = sum(psdX);
+
+cumPower = cumsum(psdX)./power;
+bandwidth = f(find(cumPower == bandwidth_criterion,1));
+
+end
+
