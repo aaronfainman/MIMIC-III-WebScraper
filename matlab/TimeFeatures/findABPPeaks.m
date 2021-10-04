a107e6f5678f68126272e19cce87411329241d08
Aaron Fainman <aaronfainman@gmail.com>
2021-10-04 15:07:23 +0200

    Faster find abp peaks

--- matlab/TimeFeatures/findABPPeaks.m
+++ matlab/TimeFeatures/findABPPeaks.m
@@ -1,17 +1,19 @@
-function [sysPts, diasPts, meanSBP, meanDBP, MAP] = findBAPPeaks(abp_wave,samp_freq)
+function [sysPts, diasPts, meanSBP, meanDBP, MAP] = findBAPPeaks(abp_wave,samp_freq, must_normalize, must_filter)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-abp_wave_norm = normalize(abp_wave);
+if(nargin==2 || must_normalize); abp_wave = normalize(abp_wave); end;
 %Chowdhury: Esimating blood pressure from the photoplethysmogram ... p.6
 %   uses a zero phase IIR butterworth filter, 6th order with fc=25 Hz
-filt_design = designfilt('lowpassiir','FilterOrder',6, ...
-    'HalfPowerFrequency',30/samp_freq,'DesignMethod','butter');
-abp_wave_norm = filtfilt(filt_design, abp_wave_norm);
+if(nargin==2 || must_filter)
+    filt_design = designfilt('lowpassiir','FilterOrder',6, ...
+        'HalfPowerFrequency',30/samp_freq,'DesignMethod','butter');
+    abp_wave = filtfilt(filt_design, abp_wave);
+end
 
-sysPts = find(islocalmax(abp_wave_norm, 'MinSeparation', 60)==1);
+sysPts = find(islocalmax(abp_wave, 'MinSeparation', 60)==1);
 
-diasPts = find( islocalmin(abp_wave_norm,'MinSeparation', 60)==1);
+diasPts = find( islocalmin(abp_wave,'MinSeparation', 60)==1);
 
 meanSBP = mean(abp_wave(sysPts));
 meanDBP = mean(abp_wave(diasPts));
