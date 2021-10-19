f88786ab2602bc3d53808ac7ae62965a53d6cb46
Basheq Tarifi <btarifi08@gmail.com>
2021-10-19 17:44:37 +0200

    gui

--- device/application/predictABP.m
+++ device/application/predictABP.m
@@ -1,17 +1,21 @@
-function abpWave = predictABP(ppgWave, nnets, normVals)
+function [abpWave, sbp, dbp, map] = predictABP(ppgWave, nnets)
 
 abpPred = predict(nnets.wave,ppgWave);
 
-sbp = predict(nnets.sbp, ppgFeats).*normVals(1) + normVals(2);
+ppgFeats = getInputFeatures(ppgWave, 125);
 
-dbp = predict(nnets.dbp, ppgFeats).*normVals(1) + normVals(2);
 
-mapVal = predict(nnets.map, ppgFeats).*normVals(1) + normVals(2);
+
+sbp = predict(nnets.sbp, ppgFeats).*nnets.abpScale + nnets.abpMean;
+
+dbp = predict(nnets.dbp, ppgFeats).*nnets.abpScale + nnets.abpMean;
+
+map = predict(nnets.map, ppgFeats).*nnets.abpScale + nnets.abpMean;
 
 mapApprox = (sbp+2*dbp)/3;
 
-if (abs(mapVal-mapApprox)/mapApprox < 0.1)
-    map = mapVal;
+if (abs(map-mapApprox)/mapApprox < 0.1)
+    map = map;
 else
     map = mapApprox;
 end
